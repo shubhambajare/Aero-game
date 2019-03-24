bfbd0a069c750a56a7f92dfb2b42e64e7d4dd1b2
shubhambajare <47632459+shubhambajare@users.noreply.github.com>
2019-03-24 23:01:12 +0530

    Update game.java

--- Src/Java/game.java
+++ Src/Java/game.java
@@ -92,3 +92,217 @@ cf = 0;
  cspeed=random.nextInt(5);
  c = new CountDownTimer(100000, 5) {
  @Override
+public void onTick(long l) {
+ if (af == 1) {
+ fire1.setVisibility(View.VISIBLE);
+y1 = y1 + 6 + aspeed + 3 * level;
+fire1.setY(y1);
+if (y1 > Sheight) {
+ y1 = 0;
+af = 0;
+fire1.setVisibility(View.INVISIBLE);
+a11 = true;
+SCORE+=1;
+ }
+if (y1 > (3 * Sheight / 4) && a11 == true)
+{
+ if (bf == 0) {
+ bf = 1;
+bspeed = random.nextInt(5);
+ } else {
+ cf = 1;
+cspeed = random.nextInt(5);
+ }
+a11 = false;
+ }
+ }
+ if (bf == 1) {
+ fire2.setVisibility(View.VISIBLE);
+y2 = y2 + 6 + bspeed + 3 * level;
+ fire2.setY(y2);
+ if (y2 > Sheight) {
+ y2 = 0;
+bf = 0;
+fire2.setVisibility(View.INVISIBLE);
+b11 = true;
+SCORE+=1;
+ }
+if (y2 > (3 * Sheight / 4) && b11 == true)
+{
+ b11 = false;
+if (af == 0) {
+ af = 1;
+aspeed = random.nextInt(5);
+ } else {
+ cf = 1;
+cspeed = random.nextInt(5);
+ }
+ }
+ }
+ if (cf == 1) {
+ fire3.setVisibility(View.VISIBLE);
+y3 = y3 + 6 + cspeed + 3 * level;
+fire3.setY(y3);
+if (y3 > Sheight) {
+ y3 = 0;cf = 0;
+fire3.setVisibility(View.INVISIBLE);
+c11 = true;
+SCORE+=1;
+ }
+if (y3 > (3 * Sheight / 4) && c11 == true)
+{
+ if (bf == 0) {
+ bf = 1;
+ bspeed = random.nextInt(5);
+ } else {
+ af = 1;
+aspeed = random.nextInt(5);
+ }
+c11 = false;
+ }
+ }
+ if ((af == 0 && bf == 0) || (af == 0 && cf ==
+0) || (cf == 0 && bf == 0)) {
+ if (af == 0)
+ af = 1;
+ else if (bf == 0)
+ bf = 1;
+ else
+ cf = 1;
+ }
+ if (decide == 2) {
+ if (x < Swidth - 2 * lengthA)
+ x =x+(6+2*(level+1));
+ i.setX(x);
+ } else if (decide == 1) {
+ if (x > 5)
+ x =x-(6+2*(level+1));
+ i.setX(x);
+ }
+ if (((Math.abs(i.getX() - fire1.getX()) < 100
+&& Math.abs(i.getY() - fire1.getY()) < 80)
+ || (Math.abs(i.getX() - fire2.getX()) <
+100 && Math.abs(i.getY() - fire2.getY()) < 80)
+ || (Math.abs(i.getX() - fire3.getX()) <
+100 && Math.abs(i.getY() - fire3.getY()) < 80))) {
+ cancel();
+ finish();
+ Intent intent = new Intent(game.this,
+gameover.class);
+ intent.putExtra("s", "" + (int) SCORE );
+startActivity(intent);
+ }
+ score.setText("SCORE: " + (int) SCORE);
+ if ((int)SCORE%11==10) {
+ SCORE+=1;
+y1 = y2 = y3 = 0;
+cf = af = 1;bf = 0;
+a11 = b11 = true;
+c11 = false;
+level++;
+tvlevel.setText("Level: " + (level + 1));
+cancel();
+fire1.setVisibility(View.INVISIBLE);
+fire2.setVisibility(View.INVISIBLE);
+fire3.setVisibility(View.INVISIBLE);
+Intent i = new Intent(game.this,
+levelup.class);
+ i.putExtra("level", "" + level);
+startActivity(i);
+ }
+ }
+ @Override
+ public void onFinish() {
+ start();
+ }
+ };
+ pause.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View view) {
+ playpause();
+ }
+ });
+ sound.setOnClickListener(new View.OnClickListener() {
+ @Override
+ public void onClick(View view) {
+ if(music.mute==true){
+ music.mute=false;
+mediaPlayer.pause();
+so.setVisibility(View.VISIBLE);
+ }else {
+ music.mute=true;
+mediaPlayer.start();
+so.setVisibility(View.INVISIBLE);
+ }
+ }
+ });
+ }
+ @Override
+ public boolean onTouchEvent(MotionEvent event) {
+ int x1=(int)event.getX();
+ switch (event.getAction()){
+ case MotionEvent.ACTION_DOWN:
+ if(x1>Swidth/2)
+ decide=2;
+ else
+ decide=1;
+ break;case MotionEvent.ACTION_MOVE:
+ if(x1>Swidth/2)
+ decide=2;
+ else
+ decide=1;
+ break;
+ case MotionEvent.ACTION_UP:
+ decide =0;
+ break;
+ }
+ return true;
+ }
+ @Override
+ public void onBackPressed() {
+ c.cancel();
+ Intent intent=new Intent(game.this,gameover.class);
+ intent.putExtra("s",""+((int)SCORE +(level*50)));
+ startActivity(intent);
+ }
+ @Override
+ protected void onPause() {
+ mediaPlayer.pause();
+ super.onPause();
+ }
+ @Override
+ protected void onResume() {
+ this.getWindow().getDecorView()
+ .setSystemUiVisibility(
+View.SYSTEM_UI_FLAG_FULLSCREEN |
+View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+ );
+ c.start();
+ if(music.mute==true) {
+ mediaPlayer.start();
+ so.setVisibility(View.INVISIBLE);
+ }else
+ so.setVisibility(View.VISIBLE);
+ super.onResume();
+ }
+ @Override
+ protected void onDestroy() {
+ mediaPlayer.pause();
+ super.onDestroy();
+ }
+ void playpause(){
+ if(pp==true) {
+ pp=false;
+ pause.setImageResource(R.drawable.p);
+ c.cancel();}
+ else {
+ pp=true;
+ pause.setImageResource(R.drawable.pause);
+ c.start();
+ }
+}
+}
